6a49916fa7d9da9f4e7eac6ae27290f269e6bfee
djes <djes1975@gmail.com>
2020-04-08 19:20:25 +0200

    Fichier de traduction français

---
+++ i18n/fr.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="fr" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="multiple_layers_edit.py" line="100"/>
+        <source>Multiple Layers Toolbar</source>
+        <translation>Barre d'outils multicouches</translation>
+    </message>
+    <message>
+        <location filename="multiple_layers_edit.py" line="103"/>
+        <source>Set edit mode for selected layers</source>
+        <translation>Mode édition pour les couches sélectionnées</translation>
+    </message>
+    <message>
+        <location filename="multiple_layers_edit.py" line="104"/>
+        <source>Commit selected layers</source>
+        <translation>Enregistre les couches sélectionnées</translation>
+    </message>
+    <message>
+        <location filename="multiple_layers_edit.py" line="105"/>
+        <source>Undo selected layers</source>
+        <translation>Annule les modifications des couches sélectionnées</translation>
+    </message>
+    <message>
+        <location filename="multiple_layers_edit.py" line="106"/>
+        <source>Filter selected layers</source>
+        <translation>Applique un filtre sur les couches</translation>
+    </message>
+    <message>
+        <location filename="multiple_layers_edit.py" line="107"/>
+        <source>Clear selected layers filter</source>
+        <translation>Enlève les filtres</translation>
+    </message>
+</context>
+</TS>
